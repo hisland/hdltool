47baf708753bcbf503f6c3e6343c460f3b3cd61b
hisland <hisland@qq.com>
2025-04-29 16:33:30 +0800

    refac: use deno2 Deno.Command

--- 03-make-zip-file.ts
+++ 03-make-zip-file.ts
@@ -1,43 +1,45 @@
-import dayjs from 'npm:dayjs'
+import dayjs from "npm:dayjs";
 
+console.error("Deno.args: ", Deno.args);
 if (Deno.args.length < 2) {
-  console.log(`必须带上2个参数 srcDir filePrefix`)
-  console.log(`deno run -A scripts/03.make-zip-file.ts srcDir filePrefix`)
-  Deno.exit(1)
+  console.error(`必须带上2个参数 srcDir filePrefix`);
+  console.error(`deno run -A scripts/03.make-zip-file.ts srcDir filePrefix`);
+  Deno.exit(1);
 }
 
-const srcDir = Deno.args[0]
-const filePrefix = Deno.args[1]
-// console.log('filePrefix: ', filePrefix)
-const today = dayjs().format('YYYYMMDD_HHmmss')
-// console.log('today : ', today)
+const srcDir = Deno.args[0];
+const filePrefix = Deno.args[1];
+// console.error('filePrefix: ', filePrefix)
+const today = dayjs().format("YYYYMMDD_HHmmss");
+// console.error('today : ', today)
 
 try {
-  const ss = Deno.statSync(srcDir)
+  const ss = Deno.statSync(srcDir);
 } catch (error) {
-  console.log(`目录不存在: ${srcDir}`)
-  Deno.exit(1)
+  console.error(`目录不存在: ${srcDir}`);
+  Deno.exit(1);
 }
 
-const fileName = `${filePrefix}_${today}.zip`
+const fileName = `${filePrefix}_${today}.zip`;
 // let fileName = ''
 // for (let ii = 1; ii < 999; ii++) {
 //   const str = `${ii}`.padStart(3, '0')
 //   fileName = `${filePrefix}_${today}_${str}.zip`
 //   try {
 //     const ss = Deno.statSync(fileName)
-//     // console.log('ss: ', ss)
+//     // console.error('ss: ', ss)
 //   } catch (error) {
 //     if (error instanceof Deno.errors.NotFound) {
 //       break
 //     }
 //   }
 // }
-// console.log('fileName: ', fileName)
+// console.error('fileName: ', fileName)
 
-const subp = Deno.run({
-  cmd: ['zip', '-r', fileName, srcDir],
-})
-const status = await subp.status()
-console.log('status: ', status)
-console.log('完成, 生成压缩包 ', fileName)
+const subp = new Deno.Command("zip", {
+  args: ["-r", fileName, srcDir],
+  stdout: "inherit",
+});
+const outObj = await subp.output();
+console.error("code: ", outObj.code);
+console.error("完成, 生成压缩包 ", fileName);
